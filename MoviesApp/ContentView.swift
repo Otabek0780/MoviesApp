aaec838de772e77f7bc00ce7b3f9afef910c2dc8
Otabek0780 <mrdunacdtm@gmail.com>
2021-03-10 14:24:09 +0900

    Initial Commit

---
+++ MoviesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesApp
+//
+//  Created by kim jong moon on 10/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
